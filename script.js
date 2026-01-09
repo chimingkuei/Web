ad6ec5a9d9c5df9b2feb1eaf818dbb8ebef66843
chimingkuei <chimingkuei@gmail.com>
2026-01-09 09:56:24 +0800

    Web

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function Hello(data){
+    document.write("<h1>"+data+"</h1>");
+}
+
+var num1 = prompt("請輸入數字1");
+var num2 = prompt("請輸入數字2");
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
+document.write(num1 + num2);
